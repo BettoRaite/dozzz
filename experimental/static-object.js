ce9297d9827dd6622b2a0e18e733386e0683bd2a
bettoraite <bettoraite@gmail.com>
2024-07-31 15:44:32 +0500

    chore: folder rename

--- experimental/static-object.js
+++
@@ -1,9 +0,0 @@
-class Rabbit extends Object {
-	constructor(name, ...args) {
-		super(...args);
-		this.name = name;
-	}
-}
-const rabbit = new Rabbit("Rab");
-
-console.log(rabbit.hasOwnProperty("name")); // Error
